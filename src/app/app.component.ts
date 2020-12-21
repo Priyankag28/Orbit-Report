63e012f75c5713032e1f58fdfd6ccf758ef1e3f8
priyanka <priyanakangel28@gmail.com>
2020-12-20 18:18:18 -0600

    part-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'orbit-report';
